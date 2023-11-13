04068b35e38173859a0dab6541b02a78281cff61
MrFrank2716 <frankiepang18@gmail.com>
2023-11-13 12:18:20 +0000

    12/11/2023

--- main.py
+++ main.py
@@ -9,18 +9,7 @@ while True:
     init = time.time()
     grabbed, frame = cap.read()
     grayFrame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-
-    # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(grayFrame, (7, 7), None)
-
-    # terminating criteria
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-    if ret == True:
-        corners2 = cv2.cornerSubPix(grayFrame, corners, (11, 11), (-1, -1), criteria)
-
-        # Draw and display the corners
-    img = cv2.drawChessboardCorners(grayFrame, (7, 7), corners, ret)
-    cv2.imshow('Chessboard', img)
+    cv2.imshow('Chessboard', grayFrame)
 
     key = cv2.waitKey(1)
     if key == 27:  # ESC
--- translate.py
+++ translate.py
@@ -5,42 +5,62 @@ import numpy as np
 cam_port = 1
 cam = ConcurrentVideoCapture(cam_port)
 result, image = cam.read()
-cv2.imwrite("Image.png", image)
+if result == True:
+    gray = cv2.convertScaleAbs(image,1)
+    result = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite("Image.png", result)
 image = cv2.imread("Image.png")
-
+def translate(image):
+    result = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite("Image.png", result)
+    image = cv2.imread("Image.png")
+# Finds the inside corners for a 8x8 board
 ret, corners = cv2.findChessboardCorners(image, (7, 7), None)
-#Paint some points in blue
-points = np.array([corners[48][0], corners[42][0], corners[6][0], corners[0][0]])
-for i in range(len(points)):
-    cv2.circle(image, tuple(points[i].astype('int64')), radius=0, color=(255, 0, 0), thickness=10)
-cv2.imwrite('Image_withPoints.png', image)
 
-#Put pixels of the chess corners: top left, top right, bottom right, bottom left.
-cornerPoints = np.array([corners[42][0], corners[0][0], corners[6][0], corners[48][0]], dtype='float32')
+# Inner Corners of the 8x8 search
+topLeft = corners[42][0]
+topRight = corners[0][0]
+btmLeft = corners[48][0]
+btmRight = corners[6][0]
 
-#Find base of the rectangle given by the chess corners
-base = np.linalg.norm(cornerPoints[1] - cornerPoints[0] )
+painted_image = cv2.circle(image, (int(topLeft[0]),int(topLeft[1])), radius=10, color=(0, 255, 0), thickness=-1)
+painted_image = cv2.circle(image, (int(topRight[0]),int(topRight[1])), radius=10, color=(0, 255, 0), thickness=-1)
+painted_image = cv2.circle(image, (int(btmLeft[0]),int(btmLeft[1])), radius=10, color=(0, 255, 0), thickness=-1)
+painted_image = cv2.circle(image, (int(btmRight[0]),int(btmRight[1])), radius=10, color=(0, 255, 0), thickness=-1)
+cv2.putText(image,"TopRight", (int(topRight[0]),int(topRight[1])), cv2.FONT_HERSHEY_TRIPLEX, 2, 255)
+cv2.putText(image,"TopLeft", (int(topLeft[0]),int(topLeft[1])), cv2.FONT_HERSHEY_TRIPLEX, 2, 255)
 
-#Height has 8 squares while base has 8 squares.
-height = base/8*8
 
-#Define new corner points from base and height of the rectangle
-new_cornerPoints = np.array([[0, 0], [int(base), 0], [int(base), int(height)], [0, int(height)]], dtype='float32')
+# Finds the outer corners of the chessboard
+xy_diff = corners[8][0] - corners[0][0] #Find the side lengths of a square
 
-#Calculate matrix to transform the perspective of the image
+topLeft[0] += xy_diff[0]
+topLeft[1] -= xy_diff[1]
+topRight[0] -= xy_diff[0]
+topRight[1] += xy_diff[1]
+btmLeft[0] += xy_diff[0]
+btmLeft[1] -= xy_diff[1]
+btmRight[0] -= xy_diff[0]
+btmRight[1] += xy_diff[1]
+
+cornerPoints = np.array([topLeft, topRight, btmRight, btmLeft], dtype='float32')
+
+painted_image = cv2.circle(image, (int(topLeft[0]),int(topLeft[1])), radius=10, color=(0, 0, 255), thickness=-1)
+painted_image = cv2.circle(image, (int(topRight[0]),int(topRight[1])), radius=10, color=(0, 0, 255), thickness=-1)
+painted_image = cv2.circle(image, (int(btmLeft[0]),int(btmLeft[1])), radius=10, color=(0, 0, 255), thickness=-1)
+painted_image = cv2.circle(image, (int(btmRight[0]),int(btmRight[1])), radius=10, color=(0, 0, 255), thickness=-1)
+cv2.imwrite("PaintedImage.png", painted_image)
+# Set the base and height of the output resolution of transformed image - 1000x1000px
+base = 1000
+height = 1000
+
+# Define new corner points from base and height of the rectangle
+new_cornerPoints = np.array([[0, 0], [base, 0], [base, height], [0, height]], dtype='float32')
+
+
+# Calculate matrix to transform the perspective of the image
 M = cv2.getPerspectiveTransform(cornerPoints, new_cornerPoints)
 
-new_image = cv2.warpPerspective(image, M, (int(base), int(height)))
-
-#Function to get data points in the new perspective from points in the image
-def calculate_newPoints(points, M):
-    new_points = np.einsum('kl, ...l->...k', M,  np.concatenate([points, np.broadcast_to(1, (*points.shape[:-1], 1)) ], axis = -1) )
-    return new_points[...,:2] / new_points[...,2][...,None]
-
-new_points = calculate_newPoints(points, M)
-
-#Paint new data points in red
-for i in range(len(new_points)):
-    cv2.circle(new_image, tuple(new_points[i].astype('int64')), radius=0, color=(0, 0, 255), thickness=5)
+new_image = cv2.warpPerspective(image, M, (base, height))
 
 cv2.imwrite('transformed.png', new_image)
